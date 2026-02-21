25c1e532c79b3527ab9522542468f54ffceafc89
子清 周 <17687952609@163.com>
2026-02-21 18:12:51 +0800

    实现角色增删改查后端api

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+
+from web.models.character import Character
 from web.models.user import UserProfile
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)   # 不能把逗号删除
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    # 外键加进来，使在管理员后台的新建和修改中，这个字段的填值方式从下拉框变成打开另一个页面进行选择(自动分页)，从而避免卡死
+    raw_id_fields = ('author',)
