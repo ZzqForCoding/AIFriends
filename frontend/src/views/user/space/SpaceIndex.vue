6c7c9b61a101de5383e0eba3ed7daaad7b5e90d5
子清 周 <17687952609@163.com>
2026-03-26 22:10:18 +0800

    实现角色卡片

--- frontend/src/views/user/space/SpaceIndex.vue
+++ frontend/src/views/user/space/SpaceIndex.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { nextTick, onMounted, onUnmounted, ref, useTemplateRef } from 'vue'
+import { nextTick, onBeforeUnmount, onMounted, onUnmounted, ref, useTemplateRef } from 'vue'
 import UserInfoField from './components/UserInfoField.vue';
 import { useRoute } from 'vue-router';
 import api from '@/js/http/api';
+import Character from '@/components/character/Character.vue';
 
 const route = useRoute()
 const userProfile = ref()
@@ -35,7 +36,6 @@ async function loadMore() {
             newCharacters = data.characters
         }
     } catch(err) {
-        console.log(err)
     } finally {
         isLoading.value = false
         if(newCharacters.length === 0) {
@@ -48,6 +48,10 @@ async function loadMore() {
             }
         }
     }
+}
+
+function removeCharacter(characterId: number) {
+    characters.value = characters.value.filter(c => c.id !== characterId)
 }
 
 let observer: IntersectionObserver | null = null
@@ -67,7 +71,7 @@ onMounted(async()=> {
     observer.observe(sentinelRef.value!)
 })
 
-onUnmounted(() => {
+onBeforeUnmount(() => {
     observer?.disconnect()
 })
 </script>
@@ -75,10 +79,16 @@ onUnmounted(() => {
 <template>
     <div class="flex flex-col items-center mb-12">
         <UserInfoField :userProfile="userProfile" />
-        <div class="grid grid-cols-[repeat(auto-fill, minmax(240px, 1fr))] gap-9 mt-12 justify-items-center w-full px-9">
-
+        <div class="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-9 mt-12 justify-items-center w-full px-9">
+            <Character
+                v-for="character in characters"
+                :key="character.id"
+                :character="character"
+                :canEdit="true"
+                @remove="removeCharacter"
+            />
         </div>
-        <div ref="sentinel-ref" class="h-2 mt-8 w-100 bg-red-500"></div>
+        <div ref="sentinel-ref" class="h-2 mt-8"></div>
         <div v-if="isLoading" class="text-gray-500 mt-4">加载中...</div>
         <div v-else-if="!hasCharacters" class="text-gray-500 mt-4">没有更多角色了</div>
     </div>
